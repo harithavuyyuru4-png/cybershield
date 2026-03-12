e69e980642182ba10a2a5a2f8ab266b18dd9506c
harithavuyyuru4-png <harithavuyyuru4@gmail.com>
2026-03-12 20:47:55 +0530

    Update api.py

--- api.py
+++ api.py
@@ -39,5 +39,6 @@ def predict():
 
 # run server
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
